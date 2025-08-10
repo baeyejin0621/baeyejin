627670faeea3abe0cf932982ba91893254932ba2
baeyejin0621 <baeyejin5652@naver.com>
2025-08-10 20:57:30 +0900

    Docs, Style: 모바일 반응형 작업 완료

--- js/click.js
+++ js/click.js
@@ -13,3 +13,26 @@ gnb.forEach((element, index) => {
     window.scrollTo({ top: section[index].offsetTop, behavior: "smooth" });
   });
 });
+
+/*브라우저 너비가 1024 이하일 때*/
+const matchMedia1024 = window.matchMedia("(max-width: 1024px)");
+const sec3Box = document.querySelectorAll(".sec3 .content .content_box");
+const sec3BoxP = document.querySelectorAll(".sec3 .content .content_box > p");
+const sec3PHeight = [];
+sec3BoxP.forEach((element) => {
+  element.style.height = "fit-content";
+  sec3PHeight.push(getComputedStyle(element).height);
+  element.style.height = 0;
+});
+
+if (matchMedia1024.matches) {
+  sec3Box.forEach((element, index) => {
+    element.addEventListener("click", () => {
+      if (getComputedStyle(sec3BoxP[index]).height == "0px") {
+        sec3BoxP[index].style.height = sec3PHeight[index];
+      } else {
+        sec3BoxP[index].style.height = 0;
+      }
+    });
+  });
+}
